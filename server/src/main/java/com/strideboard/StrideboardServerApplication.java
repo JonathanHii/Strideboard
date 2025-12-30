e83872bc36e6313717e37e6bc34475c4e00ff32a
Jonathan Hii <jhii2@outlook.com.au>
2025-12-30 13:16:57 +1000

    Set up Docker environment and initial project structure for backend and frontend

---
+++ server/src/main/java/com/strideboard/StrideboardServerApplication.java
@@ -0,0 +1,13 @@
+package com.strideboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrideboardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrideboardServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/strideboard/StrideboardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strideboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrideboardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
